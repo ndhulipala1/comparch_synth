18649aa3a7a5f63bf94bf99797ca80306028a40a
Devlin Ih <dih@olin.edu>
2022-12-04 19:21:16 -0500

    Modify square wave generator to main counter design

--- hdl/sq_wave_generator.sv
+++ hdl/sq_wave_generator.sv
@@ -1,50 +1,14 @@
 /*
-  Outputs a pulse generator with a period of "ticks".
-  out should go high for one cycle ever "ticks" clocks.
+ Takes in main period signal from channel and outputs a square wave from it.
 */
-module sq_wave_generator(clk, rst, ena, pitch_ticks, out);
+module sq_wave_generator(/*AUTOARG*/
+   // Outputs
+   square,
+   // Inputs
+   period
+   );
+   input  wire  [ 7:0] period;
+   output logic [10:0] square;
 
-parameter N = 8;
-input wire clk, rst, ena;
-input wire [N-1:0] pitch_ticks;
-output logic out;
-
-
-enum logic {
-  LOW,
-  HIGH
-} state;
-logic [N-1:0] counter;
-
-
-always_ff @(posedge clk) begin
-  if(rst) begin
-    counter <= 0;
-    state <= LOW;
-  end else if(ena) begin
-    case (state)
-      LOW: begin
-        out <= 0;
-        if (counter == pitch_ticks) begin
-          counter <= 0;
-          state <= HIGH;
-        end
-        else begin
-          counter <= counter + 1;
-        end
-      end
-      HIGH: begin
-        out <= 1;
-        if (counter == pitch_ticks) begin
-          counter <= 0;
-          state <= LOW;
-        end
-        else begin
-          counter <= counter + 1;
-        end
-      end
-    endcase
-  end
-end
-
+   always_comb square = (period[7]) ? 11'b1 : 11'b0;
 endmodule
--- tests/test_sq_wave_generator.sv
+++ tests/test_sq_wave_generator.sv
@@ -1,41 +1,24 @@
 `timescale 1ns / 1ps
 
 module test_sq_wave_generator;
+   parameter WAVE_CYCLES = 256;
 
-parameter CLK_HZ = 40_000_000;
-parameter CLK_PERIOD_NS = (1_000_000_000/CLK_HZ); // Approximation.
-parameter PERIOD_US = 10; 
-parameter CLK_TICKS = CLK_HZ*PERIOD_US/1_000_000;
+   wire [10:0] square;
+   logic [7:0] period;
+   
+   sq_wave_generator UUT(/*AUTOINST*/
+                         // Outputs
+                         .square                (square[10:0] ),
+                         // Inputs
+                         .period                (period[7:0]));
 
-logic clk, rst, ena;
-logic [$clog2(CLK_TICKS)-1:0] pitch_ticks;
-wire out;
+   initial begin
+      $dumpfile("sq_wave_generator.fst");
+      $dumpvars(0, UUT);
 
-square_wave_generator #(.N($clog2(CLK_TICKS))) UUT(
-  .clk(clk), .rst(rst), .ena(ena), .pitch_ticks(pitch_ticks), .out(out)
-);
-
-always #5 clk = ~clk;
-
-initial begin
-  $dumpfile("sq_wave_generator.fst");
-  $dumpvars(0, UUT);
-
-  rst = 1;
-  ena = 1;
-  clk = 0;
-  pitch_ticks = CLK_TICKS;
-  $display("Output a pulse ever %d (%d) ticks...", pitch_ticks, CLK_TICKS);
-  
-  repeat (2) @(negedge clk);
-  rst = 0;
-
-  repeat (4*CLK_TICKS) @(posedge clk);
-  
-  @(negedge clk);
-  ena = 0;
-  repeat (2*CLK_TICKS) @(posedge clk);
-  $finish;
-end
-
+      for (int i = 0; i < WAVE_CYCLES*2; i = i + 1) begin // Two cycles of the wave
+         period = i[7:0];
+         #1;
+      end
+   end
 endmodule
