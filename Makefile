dab6240aa9de8c384f767ef69878e1e3b4d67efa
Devlin Ih <dih@olin.edu>
2022-12-04 20:24:29 -0500

    Add testbench for channel module

--- Makefile
+++ Makefile
@@ -5,8 +5,12 @@ VVP=vvp
 VVP_POST=-fst
 VIVADO=vivado -mode batch -source
 
+# Source files for large module tests
 ILI9341_SRCS=hdl/spi_controller.sv hdl/ili9341_display_controller.sv hdl/spi_types.sv hdl/ili9341_defines.sv hdl/block_rom.sv
-FT6206_SRCS=hdl/i2c_controller.sv hdl/ft6206_controller.sv hdl/i2c_types.sv hdl/ft6206_defines.sv 
+FT6206_SRCS=hdl/i2c_controller.sv hdl/ft6206_controller.sv hdl/i2c_types.sv hdl/ft6206_defines.sv
+CHANNEL_SRCS=hdl/channel.sv hdl/sq_wave_generator.sv hdl/tri_wave_generator.sv hdl/sine_wave_generator.sv hdl/saw_wave_generator.sv hdl/clock_divider.sv
+
+
 # Add any new source files needed for the final bitstream here
 MAIN_SRCS=hdl/main.sv hdl/pulse_generator.sv hdl/pwm.sv hdl/triangle_generator.sv hdl/block_ram.sv ${ILI9341_SRCS} ${FT6206_SRCS}
 MAIN_MEMORIES=memories/ili9341_init.memh
@@ -28,6 +32,12 @@ test_saw_wave_generator: tests/test_saw_wave_generator.sv hdl/saw_wave_generator
 
 test_clock_divider: tests/test_clock_divider.sv hdl/clock_divider.sv
 	${IVERILOG} $^ -o test_clock_divider.bin && ${VVP} test_clock_divider.bin ${VVP_POST}
+
+test_channel: tests/test_channel.sv ${CHANNEL_SRCS}
+	${IVERILOG} $^ -o test_channel.bin && ${VVP} test_channel.bin ${VVP_POST}
+
+waves_channel: test_channel
+	gtkwave channel.fst -a tests/test_channel.gtkw
 
 waves_clock_divider: test_clock_divider
 	gtkwave clock_divider.fst -a tests/test_clock_divider.gtkw
---
+++ tests/test_channel.sv
@@ -0,0 +1,91 @@
+`timescale 1ns / 1ps
+
+module test_channel;
+   // Outputs
+   wire [10:0] out;
+
+   // Inputs
+   logic clk, ena, rst, rst_div;
+   logic [11:0] pitch;
+   logic [1:0]  waveform;
+
+   // Base it off the real world version
+   parameter CLOCK_FREQ = 12_000_000; // 12MHz
+   parameter NOTE_CYCLES = 100_000; // Cycles to hold each note in test
+
+   // Based on the formula for the synth pitch input
+   // pitch(freq) = (clk/(2*256*freq)) - 1
+   parameter A440 = 52;
+   parameter A880 = 26;
+
+   channel UUT(/*AUTOINST*/
+               // Outputs
+               .out                     (out[10:0]),
+               // Inputs
+               .pitch                   (pitch[11:0]),
+               .waveform                (waveform[1:0]),
+               .clk                     (clk),
+               .ena                     (ena),
+               .rst                     (rst),
+               .rst_div                 (rst_div));
+
+   always #5 clk = ~clk;
+
+   initial begin
+      $dumpfile("channel.fst");
+      $dumpvars(0, UUT);
+
+      // Reset the clock divider
+      clk = 0;
+      ena = 0;
+      rst = 1;
+      rst_div = 1;
+      waveform = 0;
+      pitch = 0;
+      repeat (2) @(negedge clk);
+
+      // Reset signals that depend on divided clock
+      rst = 0;
+      rst_div = 1;
+      pitch = 0; // Ensure minimum time to reset is taken
+      repeat (4) @(negedge clk);
+
+      // Test with A440Hz
+      rst = 0;
+      rst_div = 0;
+      ena = 1;
+      pitch = A440;
+
+      // square
+      waveform = 0;
+      repeat (NOTE_CYCLES) @(posedge clk);
+      // triangle
+      waveform = 1;
+      repeat (NOTE_CYCLES) @(posedge clk);
+      // sine
+      waveform = 2;
+      repeat (NOTE_CYCLES) @(posedge clk);
+      // saw
+      waveform = 3;
+      repeat (NOTE_CYCLES) @(posedge clk);
+
+      // Test with A880
+      pitch = A440;
+
+      // square
+      waveform = 0;
+      repeat (NOTE_CYCLES) @(posedge clk);
+      // triangle
+      waveform = 1;
+      repeat (NOTE_CYCLES) @(posedge clk);
+      // sine
+      waveform = 2;
+      repeat (NOTE_CYCLES) @(posedge clk);
+      // saw
+      waveform = 3;
+      repeat (NOTE_CYCLES) @(posedge clk);
+
+      $finish;
+   end
+
+endmodule
