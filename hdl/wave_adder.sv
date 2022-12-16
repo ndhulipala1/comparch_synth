e792893182eb7518f8e781e0519fae44e482a214
Devlin Ih <dih@olin.edu>
2022-12-16 14:57:51 -0500

    Modify wave adder to add a bus of `NUM` `N`-bit waves

--- hdl/wave_adder.sv
+++ hdl/wave_adder.sv
@@ -1,12 +1,48 @@
 `default_nettype none
 `timescale 1ns/10ps
 
-module wave_adder(channel1, channel2, out);
+/*
+ Sum up a channel bus into a 12 bit audio bus.
+ NUM must be larger than 1.
+*/
 
-// Module I/O and parameters
-input wire [10:0] channel1, channel2;
-output logic [11:0] out;
+// Warning, the following code is hard to read due to attempt to make it
+// parameterize.
+module wave_adder(/*AUTOARG*/
+   // Outputs
+   audio,
+   // Inputs
+   channels
+   );
 
-always_comb out = {1'b0, channel1} + {1'b0, channel2};
+   parameter NUM = 4; // Number of channels to add
+   parameter N   = 8; // Bus size of each channel
+
+   // Module I/O and parameters
+   input  wire  [(N*NUM)-1:0] channels;
+   output logic [11:0] audio;
+
+   // Sum them all up ripple carry style (except it's busses not bits)
+   logic [12*NUM-1:0] channels_ext, sums;
+   always_comb begin
+      // Extend first channel (subsequent ones are in generate statement)
+      channels_ext[11:0] = {{12-N{1'b0}}, channels[N-1:0]};
+      // Tie first sum to first extended channel
+      sums[11:0] = channels_ext [11:0];
+      // Last sum is the output
+      audio = sums[NUM*12-1:(NUM-1)*12];
+   end
+
+   generate
+      genvar i;
+      for (i=1; i<NUM; i++) begin
+         always_comb begin
+            // Extend ith channel to 12 bits
+            channels_ext[12*(i+1)-1:12*i] = {{(12-N){1'b0}}, channels[N*(i+1)-1:N*i]};
+            // Sum i is (i-1)th sum plus ith extended channel
+            sums[12*(i+1)-1:12*i] = sums[12*(i)-1:12*(i-1)] + channels_ext[12*(i+1)-1:12*i];
+         end
+      end
+   endgenerate
 
 endmodule
--- tests/test_wave_adder.sv
+++ tests/test_wave_adder.sv
@@ -1,36 +1,47 @@
 `timescale 1ns / 1ps
 
 module test_wave_adder;
+   // Parameters for wave adder
+   parameter NUM = 4;
+   parameter N   = 4; // 4 4-bit channels
+
    // Outputs
-   wire [11:0] out;
+   wire [11:0] audio;
 
    // Inputs
-   logic clk, ena, rst;
-   logic [10:0] channel1, channel2;
+   logic [NUM*N-1:0] channels;
 
-   wave_adder UUT(/*AUTOINST*/
-                  // Outputs
-                  .out                  (out[11:0]),
-                  // Inputs
-                  .channel1             (channel1[10:0]),
-                  .channel2             (channel2[10:0]));
+   wave_adder #(.NUM(NUM), .N(N))
+   UUT(/*AUTOINST*/
+       // Outputs
+       .audio                           (audio[11:0]),
+       // Inputs
+       .channels                        (channels[(N*NUM)-1:0]));
 
-   always #5 clk = ~clk;
 
    initial begin
       $dumpfile("wave_adder.fst");
       $dumpvars(0, UUT);
 
-      clk = 0;
-      channel1 = 11'b0;
-      channel2 = 11'b0;
+      // Set all channels to 0, output should be 0
+      channels = 0;
+      #20;
 
-      #5;
+      // Output should be 1
+      channels[3:0] = 1;
+      #20;
 
-      repeat (100_000) @(posedge clk) begin
-        channel1 = channel1 + 1;
-        channel2 = channel2 + 4;
-      end
+      // Output should be 3
+      channels[7:4] = 2;
+      #20;
+
+      // Output should be 7
+      channels[11:8] = 4;
+      #20;
+
+      // Output should be 15
+      channels[15:12] = 8;
+      #20
 
       $finish;
    end
