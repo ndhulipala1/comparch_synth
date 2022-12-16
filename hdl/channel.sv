437d5d6cdfd67312a8b790559ad553d98135fbb9
Devlin Ih <dih@olin.edu>
2022-12-16 14:58:30 -0500

    Make default wave square if input is invalid
    Done due to removal of sine wave

--- hdl/channel.sv
+++ hdl/channel.sv
@@ -90,10 +90,11 @@ module channel (/*AUTOARG*/
       end
       else begin
          case (waveform)
-           2'b00 : out = square;
-           2'b01 : out = triangle;
-         //   2'b10 : out = sine;
-           2'b10 : out = saw;
+           2'b00  : out = square;
+           2'b01  : out = triangle;
+           // 2'b10 : out = sine;
+           2'b10  : out = saw;
+           default: out = square;
          endcase
       end
    end
